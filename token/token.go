d483f65cd62e2e85618eb3c868f6c948b722e47a
Ryosuke Hanatsuka <hanatsuu@gmail.com>
2020-05-24 12:48:27 +0900

    Support 「true, false」,「if, else」, return

--- token/token.go
+++ token/token.go
@@ -1,8 +1,13 @@
 package token
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 type TokenType string
@@ -43,6 +48,11 @@ const (
 	// keyword
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 func LookupIdent(ident string) TokenType {
