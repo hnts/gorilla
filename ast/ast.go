629fd3d310ba678fd1167527f62fcfeee341722a
Ryosuke Hanatsuka <hanatsuu@gmail.com>
2020-05-26 22:31:42 +0900

    Add struct to ast.go

--- ast/ast.go
+++ ast/ast.go
@@ -1,3 +1,5 @@
+import "github.com/hnts/gorilla/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -10,4 +12,32 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name *Identifer
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifer struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifer) expressionNode {}
+func (i *Identifer) TokenLiteral() string { return ls.Token.Literal }
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	}
+	return ""
 }
