ff3f2d4b1748136807163b7c97fa00501f8bf313
Ryosuke Hanatsuka <hanatsuu@gmail.com>
2020-05-29 00:13:55 +0900

    Add main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/hnts/gorilla/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Gorilla programming language!\n",
+		user.Username)
+	fmt.Println("Feel free to type in commands")
+	repl.Start(os.Stdin, os.Stdout)
+}
