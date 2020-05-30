c475082f2c732d195795e298e8e169e745bc33d7
Ryosuke Hanatsuka <hanatsuu@gmail.com>
2020-05-30 11:38:22 +0900

    Support return statement

--- ast/ast.go
+++ ast/ast.go
@@ -43,3 +43,11 @@ func (p *Program) TokenLiteral() string {
 	}
 	return ""
 }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
