41c4e08901e39c0581f5d5db3110f9ef9464c516
Ryosuke Hanatsuka <hanatsuu@gmail.com>
2020-07-27 22:13:33 +0900

    Support to evaluate infix boolean expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -80,6 +80,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBooleanObject(left != right)
 	default:
 		return NULL
 	}
@@ -98,6 +102,14 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
